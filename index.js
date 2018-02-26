d5940fe594c0e44833877dd83ef62a28f81be579
Trevor Seitz <trevinla@gmail.com>
2018-02-26 16:30:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,15 +46,9 @@ function checkCollision(rock) {
     //            * 3. The rock's left edge is < the DODGER's right edge,
     //            *    and the rock's right edge is > the DODGER's right edge
     //            */) {
-    if (
-      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge)
-      ||
-      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge)
-      ||
-      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
-    ) {
-    return true;
-    }
+    return (
+      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
+    )
   }
 }
 
