b2326205d5fa683fe58093d1be241cb34432d1ce
Trevor Seitz <trevinla@gmail.com>
2018-02-26 17:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ function moveDodgerRight() {
    var right = parseInt(rightNumbers, 10);
 
    if (right > 0) {
-     dodger.style.right = `${left + 4}px`;
+     dodger.style.right = `${right - 4}px`;
    }
 }
 
