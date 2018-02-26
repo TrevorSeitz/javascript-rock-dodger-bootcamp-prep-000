e72da89d51884dd8f3741c0370490f56944e12b7
Trevor Seitz <trevinla@gmail.com>
2018-02-26 17:08:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -110,7 +110,8 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  gameInterval = setInterval(0)
+  // gameInterval = setInterval(0)
+  clearInterval(gameInterval)
   // ROCKS = []
   window.removeEventListener('keydown', moveDodger)
   alert = 'YOU LOSE!'
