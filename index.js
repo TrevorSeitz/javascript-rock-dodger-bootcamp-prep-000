31d8e53459a9edcdfc62aca427a8f08153eed9a6
Trevor Seitz <trevinla@gmail.com>
2018-02-26 17:32:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,11 +128,13 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    e.preventDefault()
-     if (e.which === LEFT_ARROW) {
-       moveDodgerLeft();
-     } else if (e.which === RIGHT_ARROW) {
-       moveDodgerRight();
-     }
+   e.stopPropagation()
+   
+   if (e.which === LEFT_ARROW) {
+     moveDodgerLeft();
+   } else if (e.which === RIGHT_ARROW) {
+     moveDodgerRight();
+   }
 
 }
 
